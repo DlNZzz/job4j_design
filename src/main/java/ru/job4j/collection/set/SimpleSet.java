5451704ea82af50e59a87d1263de72d2861f38a3
Dinz <kostan-82@yandex.ru>
2021-09-09 20:47:26 +0300

    7. ListIterator [#350217 #200764]

---
+++ src/main/java/ru/job4j/collection/set/SimpleSet.java
@@ -0,0 +1,36 @@
+package ru.job4j.collection.set;
+
+import ru.job4j.collection.SimpleArrayList;
+
+import java.util.Iterator;
+
+public class SimpleSet<T> implements Set<T> {
+
+    private SimpleArrayList<T> set = new SimpleArrayList<>(15);
+
+    @Override
+    public boolean add(T value) {
+        if (!contains(value)) {
+            set.add(value);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean contains(T value) {
+        for (int i = 0; i < set.size(); i++) {
+            if (value == set.get(i)
+                    || set.get(i) != null
+                    && set.get(i).equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return set.iterator();
+    }
+}
---
+++ src/test/java/ru/job4j/collection/set/SimpleSetTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.collection.set;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleSetTest {
+
+    @Test
+    public void whenAddNonNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertTrue(set.add(1));
+        assertTrue(set.contains(1));
+        assertFalse(set.add(1));
+    }
+
+    @Test
+    public void whenAddNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertTrue(set.add(null));
+        assertTrue(set.contains(null));
+        assertFalse(set.add(null));
+    }
+
+    @Test
+    public void whenAddNonNull2() {
+        Set<Integer> set = new SimpleSet<>();
+        assertTrue(set.add(1));
+        assertTrue(set.add(3));
+        assertFalse(set.contains(2));
+        assertFalse(set.add(1));
+        assertTrue(set.contains(3));
+    }
+
+    @Test
+    public void whenAddNullAndNonNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertTrue(set.add(null));
+        assertFalse(set.add(null));
+        assertTrue(set.add(1));
+        assertTrue(set.add(3));
+        assertFalse(set.contains(2));
+        assertFalse(set.add(1));
+        assertTrue(set.contains(3));
+        assertFalse(set.add(null));
+        assertTrue(set.contains(null));
+        assertFalse(set.add(null));
+    }
+
+}
