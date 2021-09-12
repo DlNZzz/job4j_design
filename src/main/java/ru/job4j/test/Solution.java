97a5ad0b541e746853aec62a03e6be10776aab6a
Dinz <kostan-82@yandex.ru>
2021-09-12 18:11:57 +0300

    1. Создать модель User [#999]

---
+++ src/main/java/ru/job4j/test/Solution.java
@@ -0,0 +1,9 @@
+package ru.job4j.test;
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = ~342;
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(277 & 432);
+    }
+}
