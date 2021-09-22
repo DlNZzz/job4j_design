59de7ab96c529373eb0e10620e2bf26e853e406c
Dinz <kostan-82@yandex.ru>
2021-09-23 00:09:30 +0300

    test

--- src/main/java/ru/job4j/test/Solution.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.test;
-
-public class Solution {
-    public static void main(String[] args) {
-        int x = ~342;
-        System.out.println(Integer.toBinaryString(x));
-        System.out.println(277 & 432);
-    }
-}
