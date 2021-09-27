a40e7f0d6c1e5d980e162d36a034f75095223136
Dinz <kostan-82@yandex.ru>
2021-09-27 18:20:43 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            out.write("Hello, world!".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            for (int i = 1; i < 6; i++) {
+                for (int j = 1; j < 6; j++) {
+                    out.write((i * j + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+            out.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
