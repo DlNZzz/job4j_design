a17ecd14a0b731bf07882af6746d28969a60db4c
Dinz <kostan-82@yandex.ru>
2021-09-27 21:48:33 +0300

    2. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -8,23 +8,26 @@ public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
         Info info = new Info(0, 0, 0);
+        Map<Integer, String> mapPrevious = new HashMap<>();
+        Map<Integer, String> mapCurrent = new HashMap<>();
         for (User user : previous) {
-            if (!current.contains(user)) {
+            mapPrevious.put(user.getId(), user.getName());
+        }
+        for (User user : current) {
+            mapCurrent.put(user.getId(), user.getName());
+        }
+        for (User user : previous) {
+            if (mapCurrent.get(user.getId()) != null) {
+                if (!user.getName().equals(mapCurrent.get(user.getId()))) {
+                    info.setChanged(info.getChanged() + 1);
+                }
+            } else {
                 info.setDeleted(info.getDeleted() + 1);
             }
         }
         for (User user : current) {
-            if (!previous.contains(user)) {
+            if (mapPrevious.get(user.getId()) == null) {
                 info.setAdded(info.getAdded() + 1);
-            }
-        }
-        for (User user : previous) {
-            for (User userCurrent : current) {
-                if (user.getId() == userCurrent.getId() && !user.getName().equals(userCurrent.getName())) {
-                    info.setChanged(info.getChanged() + 1);
-                    info.setAdded(info.getAdded() - 1);
-                    info.setDeleted(info.getDeleted() - 1);
-                }
             }
         }
         return info;
