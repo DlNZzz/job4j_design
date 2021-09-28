96e3ed0c7cc6bb7eae2da88bb9dacb17946bed89
Dinz <kostan-82@yandex.ru>
2021-09-28 23:08:07 +0300

    0.1. FileOutputStream. [#252488 #209145]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,17 +5,12 @@ import java.io.FileOutputStream;
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write("Hello, world!".getBytes());
-            out.write(System.lineSeparator().getBytes());
             for (int i = 1; i < 6; i++) {
                 for (int j = 1; j < 6; j++) {
                     out.write((i * j + " ").getBytes());
                 }
                 out.write(System.lineSeparator().getBytes());
             }
-            out.write(System.lineSeparator().getBytes());
-            out.write(10);
-            out.write(10);
         } catch (Exception e) {
             e.printStackTrace();
         }
