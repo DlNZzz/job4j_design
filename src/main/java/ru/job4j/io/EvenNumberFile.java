18cbcc65b721c35cf3bf51fc22396cdafa7060ca
Dinz <kostan-82@yandex.ru>
2021-09-28 23:20:16 +0300

    0.2. FileInputStream [#4898 #209336]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -4,18 +4,20 @@ import java.io.FileInputStream;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
+        final int LINE_FEED = 10;
+        final int CARRIAGE_RETURN = 13;
         try (FileInputStream in = new FileInputStream("even.txt")) {
             StringBuilder text = new StringBuilder();
             int num;
             int read;
             while ((read = in.read()) != -1) {
-                if (read == 13) {
+                if (read == CARRIAGE_RETURN) {
                     num = Integer.parseInt(String.valueOf(text));
                     if (num % 2 == 0) {
                         System.out.println(num);
                     }
                     text.setLength(0);
-                } else if (read != 10) {
+                } else if (read != LINE_FEED) {
                     text.append((char) read);
                 }
             }
