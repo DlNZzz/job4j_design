c01435ad73e5ad42487a4dbc0e0772492843aad6
Dinz <kostan-82@yandex.ru>
2021-09-29 19:54:32 +0300

    1. Читаем файл конфигурации [#858]

--- src/main/java/ru/job4j/Test.java
+++ src/main/java/ru/job4j/Test.java
@@ -2,13 +2,11 @@ package ru.job4j;
 
 import ru.job4j.map.Map;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Test {
     public static void main(String[] args) {
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(1, "123");
-        map.put(2, "222");
-        System.out.println(map);
+        System.out.println(Arrays.toString("qwe".getBytes()));
     }
 }
