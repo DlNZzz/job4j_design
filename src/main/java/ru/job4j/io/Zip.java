b8953d8c05c087fe438c32dfdfd69fa49c0a7da9
Dinz <kostan-82@yandex.ru>
2021-10-01 20:54:15 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,14 +1,26 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
     public static void packFiles(List<File> sources, File target) {
-
+        try (ZipOutputStream zip = new ZipOutputStream((new BufferedOutputStream(new FileOutputStream(target))))) {
+            for (File source : sources) {
+                zip.putNextEntry(new ZipEntry(source.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void packSingleFile(File source, File target) {
@@ -23,11 +35,22 @@ public class Zip {
     }
 
     public static void main(String[] args) {
-        packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );
+        if (args.length != 3) {
+            throw new IllegalArgumentException("args.length != 3");
+        }
         ArgsName argsName = ArgsName.of(args);
-        //argsName.get(0);
+        Path source = Path.of(argsName.get("d"));
+        String str = argsName.get("e");
+        File target = new File(argsName.get("o"));
+        if (source == null || !source.isAbsolute() || !target.isFile() || str == null) {
+            throw new IllegalArgumentException("no absolute or null");
+        }
+        Predicate<Path> predicate = path -> !path.toFile().getName().endsWith(str);
+        try {
+            List<File> list = Search.search(source, predicate).stream().map(Path::toFile).collect(Collectors.toList());
+            packFiles(list, target);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
