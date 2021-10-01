f3d268ed3d3d44d5cf93b2cb32f9919cc6ae207e
Dinz <kostan-82@yandex.ru>
2021-10-01 16:25:13 +0300

    кк

--- src/main/java/ru/job4j/test/task/three/SolutionTwo.java
+++ src/main/java/ru/job4j/test/task/three/SolutionTwo.java
@@ -12,22 +12,27 @@ public class SolutionTwo {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a, b, c;
         a = Integer.parseInt(reader.readLine());
-        if (a == 0) {
-            throw new IllegalArgumentException("я антон и написал 0");
-        }
         b = Integer.parseInt(reader.readLine());
         c = Integer.parseInt(reader.readLine());
-        int d = b * b - 4 * a * c;
-        if (d < 0) {
-            System.out.println("корней нет");
-        } else {
-            double x1, x2;
-            x1 = (-b + Math.sqrt(d)) / (2 * a);
-            if (d == 0) {
-                System.out.printf("x1 = %s", x1);
+        if (a == 0) {
+            if (b != 0) {
+                System.out.printf("x1 = %s", -c / b);
             } else {
-                x2 = (-b - Math.sqrt(d)) / (2 * a);
-                System.out.printf("x1 = %s\nx2 = %s", x1, x2);
+                System.out.println("корней нет");
+            }
+        } else {
+            int d = b * b - 4 * a * c;
+            if (d < 0) {
+                System.out.println("корней нет");
+            } else {
+                double x1, x2;
+                x1 = (-b + Math.sqrt(d)) / (2 * a);
+                if (d == 0) {
+                    System.out.printf("Корни одинаковые x1 = %s", x1);
+                } else {
+                    x2 = (-b - Math.sqrt(d)) / (2 * a);
+                    System.out.printf("x1 = %s\nx2 = %s", x1, x2);
+                }
             }
         }
     }
