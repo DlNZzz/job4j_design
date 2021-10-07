3cb2f2263939d9586357248272b7de7962281462
Dinz <kostan-82@yandex.ru>
2021-10-07 21:50:11 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,16 +1,20 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
     private static final String MESSAGE_CLOSE = "Exit";
     private static final String MESSAGE_HELLO = "Hello";
     private static final String MESSAGE_WHAT = "What";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -31,6 +35,8 @@ public class EchoServer {
                     out.flush();
                 }
             }
+        } catch (IOException e) {
+            LOG.error("", e);
         }
     }
 }
--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,15 +8,10 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        byte b = 1;
-        short s = 2;
-        char c = '3';
-        int age = 33;
-        long l = 4;
-        float f = 5;
-        double d = 6;
-        boolean bb = false;
-
-        LOG.debug("{} {} {} {} {} {} {} {}", b, s, c, age, l, f, d, bb);
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
     }
 }
