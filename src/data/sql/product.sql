fd78e3549e7741eb4c8e4c1af3e3934c0286c3f4
Dinz <kostan-82@yandex.ru>
2021-10-14 20:33:13 +0300

    3. Фильтры [#1732 #215709]

--- src/data/sql/product.sql
+++ src/data/sql/product.sql
@@ -16,6 +16,7 @@ select * from product;
 
 insert into type(name) values ('МОЛОКО'), ('q'), ('w'), ('e'), ('СЫР');
 insert into product(name, type_id, expired_date, price) values ('a', 13, date '2020.10.10', 120);
+insert into product(name, type_id, expired_date, price) values ('a', 15, date '2020.10.10', 120);
 insert into product(name, type_id, expired_date, price) values ('a', 16, date '2021.10.10', 420);
 insert into product(name, type_id, expired_date, price) values ('шоколадное мороженое с орехом', 16, date '2022.10.10', 420);
 insert into product(name, type_id, expired_date, price) values ('молоко1', 21, date '2021.10.10', 40);
@@ -30,24 +31,26 @@ insert into product(name, type_id, expired_date, price) values ('молоко1',
 insert into product(name, type_id, expired_date, price) values ('молоко2', 21, date '2021.10.10', 50);
 insert into product(name, type_id, expired_date, price) values ('молоко3', 21, date '2021.10.10', 40);
 insert into product(name, type_id, expired_date, price) values ('молоко2', 21, date '2021.10.10', 50);
+insert into product(name, type_id, expired_date, price) values ('молоко4', 21, date '2021.10.10', 50);
+insert into product(name, type_id, expired_date, price) values ('молоко5', 21, date '2022.10.10', 50);
 
-select * from product as p inner join type 
+select * from product as p inner join type
 on p.type_id = type.id and type.name = 'СЫР';
 
 select * from product where name LIKE '%мороженое%';
 
-select * from product where expired_date < date '2021.10.13';
+select * from product where expired_date < current_timestamp;
 
 select * from product as p where p.price = (select max(price) from product);
 
 select type.name, count(type_id) from product as p join type on p.type_id = type.id
 group by type.name;
 
-select * from product as p join type on p.type_id = type.id and (type.name = 'СЫР' or type.name = 'МОЛОКО'); 
+select * from product as p join type on p.type_id = type.id and (type.name = 'СЫР' or type.name = 'МОЛОКО');
 
-select type.name from product as p RIGHT JOIN type 
-on p.type_id = type.id 
-group by type.name 
-having count(type.name) < 10;
+select type.name, count(p.id) from product as p RIGHT JOIN type
+on p.type_id = type.id
+group by type.name
+having count(p.id) < 10;
 
 select * from product as p join type on p.type_id = type.id;
